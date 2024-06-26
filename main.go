6f129132d875909194bd56a9cdf5cda0e9e67579
kosmang <kosman2002@mail.ru>
2024-06-26 18:51:22 +0500

    creating database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	_ "modernc.org/sqlite"
 )
 
 func main() {
@@ -17,14 +20,54 @@ func main() {
 		port = "7540"
 	}
 
+	dbPath := os.Getenv("TODO_DBFILE")
+	if dbPath == "" {
+		dbPath = "scheduler.db"
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	dbFile := filepath.Join(currentDir, dbPath)
+
+	_, err = os.Stat(dbFile)
+	install := os.IsNotExist(err)
+
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer db.Close()
+
+	// если install равен true, после открытия БД требуется выполнить
+	// sql-запрос с CREATE TABLE и CREATE INDEX
+	if install {
+		createTable := `
+		CREATE TABLE IF NOT EXISTS scheduler(
+    		id INTEGER PRIMARY KEY AUTOINCREMENT,
+    		date CHAR(8) NOT NULL DEFAULT "",
+    		title VARCHAR(64) NOT NULL DEFAULT "",
+    		comment TEXT,
+    		repeat VARCHAR(128) NOT NULL DEFAULT ""
+		);
+		CREATE INDEX schedule_date ON scheduler (date);
+		`
+
+		_, err = db.Exec(createTable)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	// Директория с веб-файлами
 	webDir := "./web"
 
-	// Установим обработчик для возврата файлов из webDir
 	fileServer := http.FileServer(http.Dir(webDir))
 	r.Handle("/*", fileServer)
 
-	// Запуск веб-сервера
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal(err)
 	}
